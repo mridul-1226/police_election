9b7933498d3f73a524dbab5fd56275216dd3e7b3
mridul-1226 <mmridul1826@gmail.com>
2024-05-22 16:30:57 +0530

    Details

---
+++ lib/datas/details.dart
@@ -0,0 +1,17 @@
+List<Map<String, dynamic>> detail = [
+  {
+    'CenterName': '',
+    'PSLName': '',
+    'PSLNumber': '',
+    'SectorNumber': '',
+    'SectorName': '',
+    'SMName': '',
+    'SMContact': '',
+    'ZoneNumber': '',
+    'ZoneName': '',
+    'ZMName': '',
+    'ZMContact': '',
+    'SHO/SO Name': '',
+    'SHO/SO Contact': '',
+  },
+];
